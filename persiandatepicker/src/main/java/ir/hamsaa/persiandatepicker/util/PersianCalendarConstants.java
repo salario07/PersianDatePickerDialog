26314255283a2474e8bcd0e4c6144d949b5806d3
fardinmomen <85445785+fardinmomen@users.noreply.github.com>
2021-06-06 19:03:05 +0430

    Update PersianCalendarConstants.java

--- persiandatepicker/src/main/java/ir/hamsaa/persiandatepicker/util/PersianCalendarConstants.java
+++ persiandatepicker/src/main/java/ir/hamsaa/persiandatepicker/util/PersianCalendarConstants.java
@@ -38,18 +38,18 @@ public class PersianCalendarConstants {
 	 */
 	public static final long PERSIAN_EPOCH = 1948321;
 
-	public static final String[] persianMonthNames = { "\u0641\u0631\u0648\u0631\u062f\u06cc\u0646", // Farvardin
-			"\u0627\u0631\u062f\u06cc\u0628\u0647\u0634\u062a", // Ordibehesht
-			"\u062e\u0631\u062f\u0627\u062f", // Khordad
-			"\u062a\u06cc\u0631", // Tir
-			"\u0645\u0631\u062f\u0627\u062f", // Mordad
-			"\u0634\u0647\u0631\u06cc\u0648\u0631", // Shahrivar
-			"\u0645\u0647\u0631", // Mehr
-			"\u0622\u0628\u0627\u0646", // Aban
-			"\u0622\u0630\u0631", // Azar
-			"\u062f\u06cc", // Dey
-			"\u0628\u0647\u0645\u0646", // Bahman
-			"\u0627\u0633\u0641\u0646\u062f" // Esfand
+	public static final String[] persianMonthNames = { "حمل", // Farvardin
+			"ثور", // Ordibehesht
+			"جوزا", // Khordad
+			"سرطان", // Tir
+			"اسد", // Mordad
+			"سنبله", // Shahrivar
+			"میزان", // Mehr
+			"عقرب", // Aban
+			"قوس", // Azar
+			"جدی", // Dey
+			"دلو", // Bahman
+			"حوت" // Esfand
 	};
 
 	public static final String[] persianWeekDays = { "\u0634\u0646\u0628\u0647", // Shanbeh
